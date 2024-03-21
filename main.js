d3e55ceffaa6814e6621daeaf49ceb974cbfe2aa
lealucia <held.lea.97@googlemail.com>
2024-03-21 17:50:30 +0100

    Karte einfügen, Koordinaten ändern

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var map = L.map('map').setView([-42.411667, 173.682222], 13); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+var marker = L.marker([-42.411667, 173.682222]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
