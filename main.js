5032bde2fa7e6852ecc6f57082c670c393744110
lealucia <162601328+lealucia@users.noreply.github.com>
2024-04-09 09:46:45 +0200

    header angepasst

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-var map = L.map('map').setView([-42.411667, 173.682222], 13); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = L.map('map').setView([-42.411667, 173.682222], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
