0746fdb2412d7a35d66975aa143b3bd81a8a9cba
lealucia <held.lea.97@googlemail.com>
2024-04-11 15:55:37 +0200

    var mit let getauscht

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-var map = L.map('map').setView([-42.411667, 173.682222], 13); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let map = L.map('map').setView([-42.411667, 173.682222], 13); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
-var marker = L.marker([-42.411667, 173.682222]).addTo(map);
+let marker = L.marker([-42.411667, 173.682222]).addTo(map);
 
 marker.bindPopup("<b>Hello world!</b><br>I am Kaikoura.").openPopup();
+
