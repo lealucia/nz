ab3ed67e141a4b480c0ea01b1d671bbb0e3e92da
lealucia <held.lea.97@googlemail.com>
2024-04-11 17:37:38 +0200

    Marker als geoJSON eingebaut und Maßstab hinzugefügt

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-
-let lat= -42.411667;
-let lng= 173.682222;
-let zoom = 13; 
-
-
+let lat = -42.411667;
+let lng = 173.682222;
+let zoom = 13;
 
 let map = L.map('map', {
-    center: [lat,lng],
+    center: [lat, lng],
     zoom: zoom
 }
 );
-
-
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+let jsonPunkt = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [lng, lat]
+    },
+    "properties": {
+        "name": "Kaikoura"
+    }
+};
 
-
-
-let marker = L.marker([lat, lng]).addTo(map);
-
-marker.bindPopup(`
-<h2>Kaikoura!</h2>
-<ul>
-<li> Breite: ${lat.toFixed(5)}
-<li> Länge: ${lng.toFixed(5)}}
-</ul> 
-`).openPopup();
-
-L.control.scale({
-   imperial: false
+L.geoJSON(jsonPunkt, {}).bindPopup(function (layer) {
+    return `
+    <h2>${layer.feature.properties.name}</h2>
+    <ul>
+    <li> Breite: ${layer.feature.geometry.coordinates [1].toFixed(5)}
+    <li> Länge: ${layer.feature.geometry.coordinates [0].toFixed(5)}}
+    </ul> 
+    `;
 }).addTo(map);
